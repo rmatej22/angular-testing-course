66f501eb87444fc726eaa3ad1d9e431caf18af3a
rmatej22 <rmatej545@gmail.com>
2023-03-29 21:31:55 +0200

    create  spy for logger service & implement it

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,21 @@
 import { CalculatorService } from "./calculator.service";
 import { TestBed } from "@angular/core/testing";
+import { LoggerService } from "./logger.service";
 
 describe("CalculatorService", () => {
   let calculatorService: CalculatorService;
+  let fakeLoggerService: LoggerService;
 
   beforeEach(() => {
+    fakeLoggerService = jasmine.createSpyObj<LoggerService>("LoggerService", [
+      "log",
+    ]);
+
     TestBed.configureTestingModule({
-      providers: [CalculatorService],
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: fakeLoggerService },
+      ],
     });
 
     calculatorService = TestBed.inject<CalculatorService>(CalculatorService);
@@ -16,11 +25,15 @@ describe("CalculatorService", () => {
     const result = calculatorService.add(2, 2);
 
     expect(result).toBe(4);
+
+    expect(fakeLoggerService.log).toHaveBeenCalledTimes(1);
   });
 
   it("should subtract two numbers", () => {
     const result = calculatorService.subtract(2, 2);
 
     expect(result).toBe(0, "unexpected subtraction result");
+
+    expect(fakeLoggerService.log).toHaveBeenCalledTimes(1);
   });
 });
